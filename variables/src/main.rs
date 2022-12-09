d3694849af6c4d762ff96c84a7113103083594a0
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-12-08 23:36:28 -0600

    init commit

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,6 +5,6 @@ fn main() {
 
     let (missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
     println!("Firing {} of my {} missiles...", ready, missiles);
-    println!("{} missiles left", missiles - ready);
+    println!("{} missiles left.", missiles - ready);
 
 }
